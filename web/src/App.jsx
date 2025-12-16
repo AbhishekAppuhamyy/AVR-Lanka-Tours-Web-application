9953f96dd8dc2552fcb72a238ae45965856730c2
AbhishekAppuhamyy <abhixhek1203@gmail.com>
2025-12-16 19:52:27 +0530

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -19,6 +19,7 @@ import Dambulla from "./Destinations/dambulla.jsx";
 import AdamsPeak from "./Destinations/adams-peak.jsx";
 import Polonaruwa from "./Destinations/polonaruwa.jsx";
 import Yala from "./Destinations/yala-national-park.jsx";
+import NuwaraEliya from "./Destinations/nuwara-eliya.jsx";
 
 import Ella from "./Destinations/ella.jsx";
 
@@ -51,6 +52,7 @@ function App() {
         <Route path="/destinations/adams-peak" element={<AdamsPeak />} />
         <Route path="/destinations/polonaruwa" element={<Polonaruwa />} />
         <Route path="/destinations/yala-national-park" element={<Yala />} />
+        <Route path="/destinations/nuwara-eliya" element={<NuwaraEliya />} />
         <Route path="/destinations/ella" element={<Ella />} />
 
         {/* Packages */}
