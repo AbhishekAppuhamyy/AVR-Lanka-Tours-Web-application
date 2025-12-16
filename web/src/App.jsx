95ca680e57b9ea198cb3083f6a52d095e6d82b69
AbhishekAppuhamyy <abhixhek1203@gmail.com>
2025-12-17 00:26:52 +0530

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -33,6 +33,8 @@ import ArugamBay from "./Destinations/arugam-bay.jsx";
 import Cultural from "./Packages/cultural-triangle.jsx";
 import HillCountry from "./Packages/hill-country.jsx";
 import SouthernBeachGetaway from "./packages/southern-beach.jsx";
+import WildlifeSafariAdventure from "./packages/wildlife-safari.jsx";
+import UltimateAdventureExpedition from "./packages/adventure.jsx";
 
 
 function App() {
@@ -70,6 +72,8 @@ function App() {
         <Route path="/packages/cultural-triangle" element={<Cultural />} />
         <Route path="/packages/hill-country" element={<HillCountry />} />
         <Route path="/packages/southern-beach" element={<SouthernBeachGetaway />} />
+        <Route path="/packages/wildlife-safari" element={<WildlifeSafariAdventure />} />
+        <Route path="/packages/adventure" element={<UltimateAdventureExpedition />} />
         
       </Routes>
 
