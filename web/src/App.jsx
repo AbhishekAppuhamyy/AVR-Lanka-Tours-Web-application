9f8a56e29b0d28d249f7865d0fef5bcd0ea66176
AbhishekAppuhamyy <abhixhek1203@gmail.com>
2025-12-18 23:42:34 +0530

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,8 +1,8 @@
 // App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
+import Navbar from "./components/navbar.jsx";
+import Footer from "./components/footer.jsx";
 
 // Pages
 import Home from "./pages/Home.jsx";
